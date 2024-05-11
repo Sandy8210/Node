e3fa3a9cad62ee2e797522988c4206550b98cf5c
KGM <KGM@CASL6-37-165.kgisledu.com>
2024-05-11 08:31:51 +0530

    email not working

--- email.js
+++ email.js
@@ -1,6 +1,31 @@
-const nodeEmai = require("nodemailer");
+// const nodeEmai = require("nodemailer");
 
-const transport = nodeEmai.createTransport({
+// const transport = nodeEmai.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: "santhosgpsvg@gmail.com",
+//     pass: "Sandy_NS@200",
+//   },
+// });
+
+// const mailOptions = {
+//   from: "santhoshpsvg@gmail.com",
+//   to: "santhoshsvg8210@gmail.com",
+//   subject: "sending email from node.js",
+//   text: "haiii",
+// };
+
+// transport.sendMail(mailOptions, function (error, info) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Email sent: " + info.response);
+//   }
+// });
+
+const nodemailer = require("nodemailer");
+
+const transport = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "santhosgpsvg@gmail.com",
@@ -10,7 +35,7 @@ const transport = nodeEmai.createTransport({
 
 const mailOptions = {
   from: "santhoshpsvg@gmail.com",
-  to: "albisel786@gmail.com",
+  to: "santhoshsvg8210@gmail.com",
   subject: "sending email from node.js",
   text: "haiii",
 };
